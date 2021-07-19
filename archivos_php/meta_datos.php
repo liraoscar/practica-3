78b7104c302aecbb585ca81630df0b4774a77899
liraoscar <87486964+liraoscar@users.noreply.github.com>
2021-07-19 09:56:47 -0500

    Create meta_datos.php

---
+++ archivos_php/meta_datos.php
@@ -0,0 +1,12 @@
+<?php
+ $titulo_sitio = "Registro Simple de Usuarios";
+ $autor = "Seminario de Titulación 2021 cecytem tx.";
+ $ventanilla = "width=device-width, initial-scale=1.0";
+ $palabras clave = "registro, registro simple, registro simple de usuarios";
+ $description = "Registro Simple de Usuarios con PHP y MySQL";
+
+ $meta_nombre1 = "Autor";
+ $meta_nombre2 = "Ventanilla";
+ $meta_nombre3 = "Palabras clave";
+ $meta_nombre4 = "Descripción";
+?>
